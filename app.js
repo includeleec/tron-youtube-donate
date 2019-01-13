147f8df19832400f5d4559dba3681e7ec26ed6e8
leec <includeleec@gmail.com>
2019-01-13 12:15:52 +0800

    add hello

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var app = express();
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
-
+app.get('/hello', (req, res) => res.send('Hello World!'))
 
 app.listen(80, function () {
   console.log('Example app listening on port 80!');
