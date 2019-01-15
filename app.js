b63cb6418c00a39cc4ade982e5bcbea70605c959
leec <includeleec@gmail.com>
2019-01-15 18:04:30 +0800

    change port

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 var express = require('express');
 var path = require('path');
 var app = express();
-const PORT = 3000
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('/hello', (req, res) => res.send('Hello World!'))
+app.get('/hello', (req, res) => res.send('Hello World!'));
 
-app.listen(PORT,function () {
-  console.log('Example app listening on port '+ PORT);
-});
+app.set('port', process.env.PORT || 80);
+
+app.listen(app.get('port'))
